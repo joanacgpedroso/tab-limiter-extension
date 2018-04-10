aa24472f31cea611e1c14de52e6618267e869a56
Joana Pedroso <joana.pedroso@computedlife.com>
2018-04-10 10:26:18 +0100

    Add Standard JS

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,41 +1,41 @@
 /* global browser */
 
-const
-    defaultMaxTabs = 5,
-    form = document.querySelector("form");
-//Fill settings with values when loaded
-document.addEventListener("DOMContentLoaded", FillWithValues)
+const defaultMaxTabs = 5
+const form = document.querySelector('form')
 
-//Save new settings when form submitted
-form.addEventListener("submit", SaveSettings);
+// Fill settings with values when loaded
+document.addEventListener('DOMContentLoaded', FillWithValues)
 
-//Fill fields with correct info
-function FillWithValues() {
-    browser.storage.local.get()
-        .then((settingsValues) => {
-            //Max tabs
-            let maxTabs = MaxTabs(settingsValues.maxTabs);
-            form.querySelector("#maxTabs").value = maxTabs;
-        })
-        .catch((error) => console.error(error))
-}
+// Save new settings when form submitted
+form.addEventListener('submit', SaveSettings)
 
-//Save new settings
-function SaveSettings(event) {
-    event.preventDefault();
-    browser.storage.local.set({
-        maxTabs: parseInt(document.querySelector("#maxTabs").value) || defaultMaxTabs
+// Fill fields with correct info
+function FillWithValues () {
+  browser.storage.local.get()
+    .then((settingsValues) => {
+      let maxTabs = MaxTabs(settingsValues.maxTabs)
+
+      form.querySelector('#maxTabs').value = maxTabs
     })
-    FillWithValues();
+    .catch((error) => console.error(error))
 }
 
-function MaxTabs(settingValue) {
-    let
-        biggerThanZero = 0,
-        maxTabs = defaultMaxTabs;
-    if (settingValue && settingValue > biggerThanZero) {
-        maxTabs = settingValue;
-    }
+// Save new settings
+function SaveSettings (event) {
+  event.preventDefault()
+  browser.storage.local.set({
+    maxTabs: parseInt(document.querySelector('#maxTabs').value) || defaultMaxTabs
+  })
+  FillWithValues()
+}
 
-    return maxTabs;
+function MaxTabs (settingValue) {
+  let biggerThanZero = 0
+  let maxTabs = defaultMaxTabs
+
+  if (settingValue && settingValue > biggerThanZero) {
+    maxTabs = settingValue
+  }
+
+  return maxTabs
 }
