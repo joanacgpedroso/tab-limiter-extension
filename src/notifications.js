26f5aa082886a4a3fd48f85581b9a2cd765e2cf7
Joana Pedroso <26660640+joanacgpedroso@users.noreply.github.com>
2018-04-13 15:25:50 +0100

    Add icon to notification

--- src/notifications.js
+++ src/notifications.js
@@ -3,6 +3,7 @@
 export function ShowNotification (title, message) {
   return browser.notifications.create({
     type: 'basic',
+    iconUrl: browser.extension.getURL('../src/assets/icons/tab-icon-google.svg'),
     title: title,
     message: message
   })
