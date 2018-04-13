cea577cf953f59fd0e4594fa140f4e3e5b1dd4e1
Joana Pedroso <joana.pedroso@computedlife.com>
2018-04-13 09:11:01 +0100

    Add popup files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
   devtool: 'inline-source-map',
   entry: {
     background: './src/app.js',
-    options: './src/options/options.js'
+    options: './src/options/options.js',
+    popup: './src/popup/popup.js'
   },
   output: {
     filename: '[name].js',
