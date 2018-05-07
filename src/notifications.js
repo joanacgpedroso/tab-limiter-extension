b1d5a9dc20924a7e8bc7fa4b8aabcd3371911759
Joana Pedroso <26660640+joanacgpedroso@users.noreply.github.com>
2018-05-07 16:20:14 +0100

    Fix bug #2

--- src/notifications.js
+++ src/notifications.js
@@ -1,10 +1,15 @@
 /* global browser */
+const notificationId = 'browser-notification'
 
 export function ShowNotification (title, message) {
-  return browser.notifications.create({
+  return browser.notifications.create(notificationId, {
     type: 'basic',
     iconUrl: browser.extension.getURL('./dist/assets/icons/tab-icon-google.svg'),
     title: title,
     message: message
   })
+}
+
+export function CloseNotification () {
+  return browser.notifications.clear(notificationId)
 }
